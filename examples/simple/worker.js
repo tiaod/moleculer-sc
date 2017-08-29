688098265f0f2f3c44668003c8f85c444825df9d
tiaod <tiaod@qq.com>
2017-08-29 21:38:24 +0800

    publish to npm

--- examples/simple/worker.js
+++ examples/simple/worker.js
@@ -46,31 +46,4 @@ module.exports.run = function (worker) {
   healthChecker.attach(worker, app);
 
   httpServer.on('request', app);
-
-  var count = 0;
-
-  /*
-    In here we handle our incoming realtime connections and listen for events.
-  */
-  scServer.on('connection', function (socket) {
-
-    // Some sample logic to show how to handle client events,
-    // replace this with your own logic
-
-    socket.on('sampleClientEvent', function (data) {
-      count++;
-      console.log('Handled sampleClientEvent', data);
-      scServer.exchange.publish('sample', count);
-    });
-
-    var interval = setInterval(function () {
-      socket.emit('rand', {
-        rand: Math.floor(Math.random() * 5)
-      });
-    }, 1000);
-
-    socket.on('disconnect', function () {
-      clearInterval(interval);
-    });
-  });
 };
